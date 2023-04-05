ac1ff6f02747c78242f2e8367adbcdb2486b0e08
wickeddd3 <pablophilipandrew@gmail.com>
2023-04-05 22:57:31 +0800

    add textfield component

---
+++ src/core/fields/TextField.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="col-span">
+    <label
+      for="title"
+      class="block text-sm font-medium leading-6 text-gray-900"
+    >
+      {{ title }}
+    </label>
+    <div class="mt-2">
+      <input
+        :value="value"
+        :name="name"
+        :id="name"
+        :autocomplete="name"
+        type="text"
+        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        @input="$emit('input', $event)"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  title?: string
+  value?: string,
+  name?: string,
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  title: '',
+  value: '',
+  name: '',
+})
+</script>
+
+<style scoped>
+
+</style>

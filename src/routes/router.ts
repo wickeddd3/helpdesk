b6f6fb2d5780b0235ad91f0f029958b845f2daef
wickeddd3 <pablophilipandrew@gmail.com>
2023-04-10 15:09:54 +0800

    add route navigation guards

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,4 +1,5 @@
 import * as vR from 'vue-router';
+import { reactive } from 'vue';
 import DashboardPage from '@pages/Dashboard.vue';
 import NotFoundPage from '@pages/NotFound.vue';
 
@@ -7,48 +8,79 @@ const _routes:Array<vR.RouteRecordRaw> = [
   {
     path: '/',
     component: DashboardPage,
-    name: 'home'
+    name: 'home',
+    meta: {
+      authOnly: true,
+    },
   },
   {
     path: '/tickets',
     component: () => import("@/pages/Tickets/index.vue"),
     name: 'home.tickets',
+    meta: {
+      authOnly: true,
+    },
   },
   {
     path: '/tickets/:id',
     component: () => import("@/pages/Tickets/Ticket.vue"),
     name: 'home.tickets.ticket',
+    meta: {
+      authOnly: true,
+    },
   },
   {
     path: '/tickets/incident/submit',
     component: () => import("@/pages/Tickets/SubmitIncident.vue"),
     name: 'home.tickets.incident.submit',
+    meta: {
+      authOnly: true,
+    },
   },
   {
     path: '/tickets/request/submit',
     component: () => import("@/pages/Tickets/SubmitRequest.vue"),
     name: 'home.tickets.request.submit',
+    meta: {
+      authOnly: true,
+    },
   },
   {
     path: '/profile',
     component: () => import("@/pages/Profile.vue"),
     name: 'home.profile',
+    meta: {
+      authOnly: true,
+    },
   },
   {
     path: '/login',
     component: () => import("@pages/Auth.vue"), // Lazy loading to defer loading only when required
-    name: 'home.auth', // user defined. Keep dot notation to have elegant design
+    name: 'auth', // user defined. Keep dot notation to have elegant design
+    meta: {
+      guestOnly: true,
+    },
   },
   {
     path: '/:pathMatch(.*)*',
     component: NotFoundPage,
     name: 'home.missing',
+    meta: {
+      authOnly: true,
+    },
   },
 ];
 
 const router = vR.createRouter({
   history: vR.createWebHistory(), // WebHash  history will add hash before every route
   routes: _routes, // Send the routes here.
+});
+
+router.beforeEach((to, from, next) => {
+  const auth = localStorage.getItem('helpdesk.auth');
+  if (to.meta.authOnly && !auth) next({ name: 'auth' })
+  else if (to.meta.guestOnly && auth) next({ name: 'home' })
+  else next();
 });
 
 export default router;

867a6047c0e4cd42871cdff2bbd3684b9d8501e3
wickeddd3 <pablophilipandrew@gmail.com>
2023-04-16 13:01:23 +0800

    conditionally set base router path

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "./",
+  base: process.env.NODE_ENV === "production" ? "/helpdesk/" : "./",
   resolve: {
     alias: {
       // Two methods available
